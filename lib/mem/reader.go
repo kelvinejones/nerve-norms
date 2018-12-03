02061d3416061765f7bbd8bb7ab5c997b33126fa
James Bell <james@stellentus.com>
2018-12-03 10:08:26 -0700

    Allow a string to be written back into Reader's buffer

--- lib/mem/reader.go
+++ lib/mem/reader.go
@@ -2,12 +2,16 @@ package mem
 
 import (
 	"bufio"
+	"errors"
 	"io"
 	"strings"
 )
 
 type Reader struct {
-	reader *bufio.Reader
+	reader             *bufio.Reader
+	unwrittenString    string
+	useUnwrittenString bool
+	writeError         bool
 }
 
 func NewReader(rd io.Reader) *Reader {
@@ -18,6 +22,22 @@ func NewStringReader(str string) *Reader {
 	return &Reader{reader: bufio.NewReader(strings.NewReader(str))}
 }
 
+func (rd *Reader) UnwriteString(str string) {
+	// If this is called twice without a call to ReadString in between, the next ReadString call will error.
+	if rd.useUnwrittenString {
+		rd.writeError = true
+	}
+	rd.unwrittenString = str
+	rd.useUnwrittenString = true
+}
+
 func (rd *Reader) ReadString(delim byte) (string, error) {
-	return rd.reader.ReadString(delim)
+	if rd.writeError {
+		return rd.unwrittenString, errors.New("UnwriteString was called twice before ReadString was called")
+	}
+	if rd.useUnwrittenString {
+		return rd.unwrittenString, nil
+	} else {
+		return rd.reader.ReadString(delim)
+	}
 }
