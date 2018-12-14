efc2bd4b75f8da2677e51bbe39fc4a8bd856f9a3
James Bell <james@stellentus.com>
2018-12-14 15:33:27 -0700

    Add documentation

--- lib/mem/types.go
+++ lib/mem/types.go
@@ -15,7 +15,12 @@ type XYZ struct {
 
 type LineParser interface {
 	LinePrefix() string
+
+	// ParseRegex provides a regexp used to split a line.
 	ParseRegex() *regexp.Regexp
+
+	// ParseLine parses a line that was split by the Regexp.
+	// `err` might be non-nil even if `keepParsing` is true; it's not a terminating error.
 	ParseLine([]string) error
 }
 
