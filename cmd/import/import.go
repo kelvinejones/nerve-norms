e2b45af7405b73e3fb297fe214676369a0fd3290
James Bell <james@stellentus.com>
2018-12-14 14:14:06 -0700

    Import an entire directory at once

--- cmd/import/import.go
+++ cmd/import/import.go
@@ -5,19 +5,34 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"gogs.bellstone.ca/james/jitter/lib/mem"
 )
 
-var path = flag.String("path", "res/data/short_test.MEM", "path to the file that should be imported")
+var path = flag.String("path", "res/data/short_test.MEM", "path to the file or directory that should be imported")
 
 func main() {
 	flag.Parse()
 
-	err := printMem(*path)
-	if err != nil {
-		fmt.Println("Could not parse '" + *path + "' due to error: " + err.Error())
-	}
+	// This works regardless of whether *path is a file or a directory, though if it's an invalid file, it will be silently skipped
+	filepath.Walk(*path, func(subpath string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println("Could not walk '" + subpath + "' due to error: " + err.Error())
+			return nil
+		}
+
+		if !strings.Contains(subpath, ".MEM") || info.Mode().IsDir() {
+			// Skip directories and invalid files
+			return nil
+		}
+
+		if err := printMem(subpath); err != nil {
+			fmt.Println("Could not parse '" + subpath + "' due to error: " + err.Error())
+		}
+		return nil
+	})
 }
 
 func printMem(path string) error {
