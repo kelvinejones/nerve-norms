96d88372fc7603b1f3910536bc924497fb3e78ff
James Bell <james@stellentus.com>
2018-12-14 13:59:24 -0700

    Add doc file

---
+++ lib/mem/doc.go
@@ -0,0 +1,13 @@
+package mem
+
+/*
+
+The mem package is used to load and store MEM files.
+* `mem.go` contains the main type for MEM files and code to import them. `mem.Import` is the main function to be used from this package.
+* `reader.go` reads a MEM file line by line.
+* `types.go` contains miscellaneous types.
+* The other files implement necessary functions to parse a single section of a MEM file.
+
+This package currently exports a lot of things that shouldn't be exported.
+
+*/
