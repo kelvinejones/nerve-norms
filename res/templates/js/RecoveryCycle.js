d857533e6cdb20ae51aa95b5dd216bd4c760dc65
Matthias Stone <matthias@bellstone.ca>
2019-01-29 01:51:01 -0700

    Draw confidence interval for recovery cycle

--- res/templates/js/RecoveryCycle.js
+++ res/templates/js/RecoveryCycle.js
@@ -17,16 +17,39 @@ class RecoveryCycle extends Chart {
 			.x(function(d) { return self.x(d.delay); })
 			.y(function(d) { return self.y(d.value); });
 
-		// Add the valueline path.
+		const meanline = d3.line()
+			.x(function(d) { return self.x(d.delay); })
+			.y(function(d) { return self.y(d.mean); });
+
+		const intervalData = (Array.from(this.data)
+				.map(function(d) { return { x: d.delay, y: d.mean + 3 * d.SD } }))
+			.concat(Array.from(this.data).reverse().map(function(d) { return { x: d.delay, y: d.mean - 3 * d.SD } }))
+		const interval = d3.line()
+			.x(function(d) { console.log(d); return self.x(d.x); })
+			.y(function(d) { return self.y(d.y); });
+
+		// Draw the confidence interval
+		svg.append("path")
+			.data([intervalData])
+			.attr("class", "confidenceinterval")
+			.attr("d", interval);
+
 		svg.append("path")
 			.data([this.data])
-			.attr("class", "line")
-			.attr("d", valueline);
+			.attr("class", "meanline")
+			.attr("d", meanline);
 
+		// Add a reference line for 0
 		svg.append("path")
 			.data([
 				[{ "delay": 1, "value": 0 }, { "delay": 200, "value": 0 }]
 			])
+			.attr("class", "meanline")
+			.attr("d", valueline);
+
+		// Add the valueline path.
+		svg.append("path")
+			.data([this.data])
 			.attr("class", "line")
 			.attr("d", valueline);
 
