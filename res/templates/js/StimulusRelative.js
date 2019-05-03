514b2bd6dc917c2ac626cc6f2a6a516fa03b381b
James Bell <james@bellstone.ca>
2019-05-03 12:38:42 -0600

    Add SD area for SR relative

--- res/templates/js/StimulusRelative.js
+++ res/templates/js/StimulusRelative.js
@@ -4,15 +4,26 @@ class StimulusRelative extends Chart {
 		this.data = this.calculateData(plots.sr.data)
 		this.xName = 'x'
 		this.yName = 'y'
-		this.limFunc = undefined
+		this.xSDName = 'SD'
+		this.ySDName = undefined
+		this.xMeanName = 'mean'
+		this.yMeanName = undefined
+		this.limFunc = this.sdAtLoc // Only sd lines are supported
 	}
 
 	calculateData(data) {
 		const stimFor50PercentMax = data[24].valueX // Could also be extracted from excitability variables
+		const meanStimFor50PercentMax = data[24].meanX
+
+		function norm(a, b) { return Math.sqrt(a * a + b * b); }
+
 		return data.map((d, i) => {
 			return {
-				'x': d.valueX / stimFor50PercentMax * 100, // Normalize each element
 				'y': (i + 1) * 2,
+				// Normalize each element
+				'x': d.valueX / stimFor50PercentMax * 100,
+				'SD': norm(d.SDX, d.SDY) / d.meanX * 100,
+				'mean': d.meanX / meanStimFor50PercentMax * 100,
 			}
 		})
 	}
