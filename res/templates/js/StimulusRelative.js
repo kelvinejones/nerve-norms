5289bcfe83717b8501d05725abbe9389a24d5e0b
James Bell <james@bellstone.ca>
2019-05-03 09:02:27 -0600

    Create function to do SR-rel calculations

--- res/templates/js/StimulusRelative.js
+++ res/templates/js/StimulusRelative.js
@@ -1,15 +1,19 @@
 class StimulusRelative extends Chart {
 	constructor(plots) {
 		super([0, 200], [0, 100])
-		const stimFor50PercentMax = plots.sr.data[24].valueX // Could also be extracted from excitability variables
-		this.data = plots.sr.data.map(function callback(d, i) {
+		this.data = this.calculateData(plots.sr.data)
+		this.xName = 'x'
+		this.yName = 'y'
+	}
+
+	calculateData(data) {
+		const stimFor50PercentMax = data[24].valueX // Could also be extracted from excitability variables
+		return data.map(function callback(d, i) {
 			return {
 				'x': d.valueX / stimFor50PercentMax * 100, // Normalize each element
 				'y': (i + 1) * 2,
 			}
 		})
-		this.xName = 'x'
-		this.yName = 'y'
 	}
 
 	get name() { return "Relative Stimulus Response" }
