420c1dca985efbd49bfc190d8a91dc264f4e4a72
James Bell <james@bellstone.ca>
2019-05-03 10:40:56 -0600

    Simplify TE code by iterating through plots

--- res/templates/js/ThresholdElectrotonus.js
+++ res/templates/js/ThresholdElectrotonus.js
@@ -1,11 +1,7 @@
 class ThresholdElectrotonus extends Chart {
 	constructor(plots) {
 		super([0, 200], [-150, 100])
-
-		this.hy40 = plots.te.data.h40
-		this.de40 = plots.te.data.d40
-		this.hy20 = plots.te.data.h20
-		this.de20 = plots.te.data.d20
+		this.data = plots.te.data
 	}
 
 	get name() { return "Threshold Electrotonus" }
@@ -13,28 +9,21 @@ class ThresholdElectrotonus extends Chart {
 	get yLabel() { return "Delay (ms)" }
 
 	updatePlots(plots) {
-		this.hy40 = plots.te.data.h40
-		this.de40 = plots.te.data.d40
-		this.hy20 = plots.te.data.h20
-		this.de20 = plots.te.data.d20
+		this.data = plots.te.data
 		this.animateLines()
 	}
 
 	drawLines(svg) {
-		this.createXYLineWithMean(this.hy40, "hy40")
-		this.createXYLineWithMean(this.de40, "de40")
-		this.createXYLineWithMean(this.hy20, "hy20")
-		this.createXYLineWithMean(this.de20, "de20")
-
+		Object.keys(this.data).forEach(key => {
+			this.createXYLineWithMean(this.data[key], key)
+		})
 		this.drawHorizontalLine(this.linesLayer, 0)
-
 		this.animateLines()
 	}
 
 	animateLines() {
-		this.animateXYLineWithMean(this.hy40, "hy40")
-		this.animateXYLineWithMean(this.de40, "de40")
-		this.animateXYLineWithMean(this.hy20, "hy20")
-		this.animateXYLineWithMean(this.de20, "de20")
+		Object.keys(this.data).forEach(key => {
+			this.animateXYLineWithMean(this.data[key], key)
+		})
 	}
 }
