3449b1a757a91e57e464f5023a17e0b9ab9926ba
James Bell <james@bellstone.ca>
2019-05-11 19:06:44 -0600

    Split code for excitability variable display into a file

---
+++ res/templates/js/ExVars.js
@@ -0,0 +1,54 @@
+class ExVars {
+	static _interpolate() {
+		const opacity = 0.8,
+			red = d3.hsl("red"),
+			green = d3.hsl("lightgreen");
+
+		red.opacity = opacity
+		green.opacity = opacity
+
+		const intr = d3.interpolateHsl(green, red);
+		return function(score) { return intr(Math.pow(score, 3)) }
+	}
+
+	static _setExcitabilityVariable(idString, value, score) {
+		const row = document.getElementById(idString);
+		row.getElementsByClassName("excite-value")[0].innerHTML = value
+		if (score !== undefined) {
+			row.style.background = "linear-gradient(to right, " + ExVars._interpolate(score) + " " + score * 100 + "%, #ffffff 0%)"
+		}
+	}
+
+	static _setHeaderScore(str, score) {
+		[...document.querySelectorAll(str)].forEach(elm => {
+			elm.style.background = ExVars._interpolate(score);
+		})
+	}
+
+	static _labelForScore(score) {
+		if (score < 0.75) {
+			return "Healthy"
+		} else if (score < .95) {
+			return "Abnormal"
+		} else {
+			return "Extremely Abnormal"
+		}
+	}
+
+	static update(data) {
+		ExVars._setHeaderScore(".participant-header", data.outlierScore)
+		const healthLabel = ExVars._labelForScore(data.outlierScore)
+		ExVars._setExcitabilityVariable("overall-score", healthLabel + " (" + data.outlierScore.toFixed(2) + ")", 0)
+		const nameSpan = document.getElementById("participant-name");
+		nameSpan.innerHTML = data.participant + " (" + healthLabel + ")"
+
+		Object.keys(data.plots).map(function(key) {
+				ExVars._setHeaderScore("." + key + "-header", data.plots[key].outlierScore)
+				return data.plots[key].discreteMeasures;
+			}).flat()
+			.concat(data.discreteMeasures)
+			.forEach(function(exind) {
+				ExVars._setExcitabilityVariable("qtrac-excite-" + exind.qtracExciteID, exind.value, exind.outlierScore)
+			})
+	}
+}
--- res/templates/js/init.js
+++ res/templates/js/init.js
@@ -27,73 +27,17 @@ function initPlots(data) {
 	})
 
 	// Now set all excitability variables
-	updateIndices(data);
+	ExVars.update(data);
 
 	function changeParticipant(ev) {
 		plots.forEach(pl => {
 			currentParticipant = participants[ev.srcElement.value] // This is a global
 			pl.chart.updatePlots(currentParticipant.plots)
 			pl.chart.updateNorms(currentParticipant.plots)
-			updateIndices(currentParticipant)
+			ExVars.update(currentParticipant)
 		})
 	}
 
 	document.getElementById("select-participant-dropdown")
 		.addEventListener("change", changeParticipant);
-}
-
-function updateIndices(data) {
-	const interpolate = function() {
-		const opacity = 0.8,
-			red = d3.hsl("red"),
-			green = d3.hsl("lightgreen");
-
-		red.opacity = opacity
-		green.opacity = opacity
-
-		const intr = d3.interpolateHsl(green, red);
-		return function(score) { return intr(Math.pow(score, 3)) }
-	}()
-
-	function setExcitabilityVariable(idString, value, score) {
-		const row = document.getElementById(idString);
-		row.getElementsByClassName("excite-value")[0].innerHTML = value
-
-		if (score !== undefined) {
-			percent = score * 100
-			color = interpolate(score)
-			row.style.background = "linear-gradient(to right, " + color + " " + percent + "%, #ffffff 0%)"
-		}
-	}
-
-	function setHeaderScore(str, score) {
-		[...document.querySelectorAll(str)].forEach(elm => {
-			elm.style.background = interpolate(score);
-		})
-	}
-
-	function labelForScore(score) {
-		if (score < 0.75) {
-			return "Healthy"
-		} else if (score < .95) {
-			return "Abnormal"
-		} else {
-			return "Extremely Abnormal"
-		}
-	}
-
-	setHeaderScore(".participant-header", data.outlierScore)
-	const healthLabel = labelForScore(data.outlierScore)
-	setExcitabilityVariable("overall-score", healthLabel + " (" + data.outlierScore.toFixed(2) + ")", 0)
-	const nameSpan = document.getElementById("participant-name");
-	nameSpan.innerHTML = data.participant + " (" + healthLabel + ")"
-
-	Object.keys(data.plots).map(function(key) {
-			setHeaderScore("." + key + "-header", data.plots[key].outlierScore)
-			return data.plots[key].discreteMeasures;
-		}).flat()
-		.concat(data.discreteMeasures)
-		.forEach(function(exind) {
-			setExcitabilityVariable("qtrac-excite-" + exind.qtracExciteID, exind.value, exind.outlierScore)
-		})
 }
