c83a038d630c0acf3ae91c5a86177ec3bc517f0e
James Bell <james@bellstone.ca>
2019-05-13 14:19:51 -0600

    Allow charts to be built

--- res/templates/js/ChartFactory.js
+++ res/templates/js/ChartFactory.js
@@ -8,7 +8,7 @@ class ChartFactory {
 			},
 		}
 
-		this.partDropDown = new DataDropDown("select-participant-dropdown", participants, function(name, currentParticipant) {
+		this.partDropDown = new DataDropDown("select-participant-dropdown", participants, (name, currentParticipant) => {
 			this.osAccessor.participant = name
 			ExVars.update(this.osAccessor.getScores(), currentParticipant);
 			plots.forEach(pl => {
@@ -16,7 +16,7 @@ class ChartFactory {
 			})
 		})
 
-		this.normDropDown = new DataDropDown("select-normative-dropdown", norms, function(name, currentNormative) {
+		this.normDropDown = new DataDropDown("select-normative-dropdown", norms, (name, currentNormative) => {
 			this.osAccessor.normative = name
 			ExVars.update(this.osAccessor.getScores(), this.partDropDown.data);
 			plots.forEach(pl => {
@@ -25,26 +25,26 @@ class ChartFactory {
 		})
 
 		this.osAccessor.participant = this.partDropDown.selection
-		this.osAccessor.normative this.normDropDown.selection
+		this.osAccessor.normative = this.normDropDown.selection
 
 		// Create all of the plots
 		const plots = [{
-			chart: new RecoveryCycle(this.partDropDown.data.plotsthis.normDropDown.data.plots),
+			chart: this.build("rc"),
 			selector: "#recoveryCycle svg",
 		}, {
-			chart: new ThresholdElectrotonus(this.partDropDown.data.plotsthis.normDropDown.data.plots),
+			chart: this.build("te"),
 			selector: "#thresholdElectrotonus svg",
 		}, {
-			chart: new ChargeDuration(this.partDropDown.data.plotsthis.normDropDown.data.plots),
+			chart: this.build("cd"),
 			selector: "#chargeDuration svg",
 		}, {
-			chart: new ThresholdIV(this.partDropDown.data.plotsthis.normDropDown.data.plots),
+			chart: this.build("tiv"),
 			selector: "#thresholdIV svg",
 		}, {
-			chart: new StimulusResponse(this.partDropDown.data.plotsthis.normDropDown.data.plots),
+			chart: this.build("sr"),
 			selector: "#stimulusResponse svg",
 		}, {
-			chart: new StimulusRelative(this.partDropDown.data.plotsthis.normDropDown.data.plots),
+			chart: this.build("srel"),
 			selector: "#stimulusResponseRelative svg",
 		}, ]
 
@@ -56,5 +56,22 @@ class ChartFactory {
 
 		// Now set all excitability variables
 		ExVars.update(this.osAccessor.getScores(), this.partDropDown.data);
+	}
+
+	build(typeStr) {
+		switch (typeStr) {
+			case "rc":
+				return new RecoveryCycle(this.partDropDown.data.plots, this.normDropDown.data.plots)
+			case "te":
+				return new ThresholdElectrotonus(this.partDropDown.data.plots, this.normDropDown.data.plots)
+			case "cd":
+				return new ChargeDuration(this.partDropDown.data.plots, this.normDropDown.data.plots)
+			case "tiv":
+				return new ThresholdIV(this.partDropDown.data.plots, this.normDropDown.data.plots)
+			case "sr":
+				return new StimulusResponse(this.partDropDown.data.plots, this.normDropDown.data.plots)
+			case "srel":
+				return new StimulusRelative(this.partDropDown.data.plots, this.normDropDown.data.plots)
+		}
 	}
 }
