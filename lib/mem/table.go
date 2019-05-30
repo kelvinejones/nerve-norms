ba079c80435b6db72aa683a5d4945e7a90e32c7b
James Bell <james@bellstone.ca>
2019-05-30 10:22:10 -0600

    Split table and column

--- lib/mem/table.go
+++ lib/mem/table.go
@@ -4,12 +4,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"math"
 	"regexp"
 	"strconv"
 )
 
-type Column []float64
 type Table []Column
 type TableSet struct {
 	// ColCount is the number of columns in Names and in each Table.
@@ -23,62 +21,6 @@ type TableSet struct {
 
 	// Abbreviation is the prefix of each import row.
 	Abbreviation string
-}
-
-func (val *Column) ImputeWithValue(oldLabel, newLabel Column, eps float64, logX bool) Column {
-	num := len(newLabel)
-	col := Column(make([]float64, num))
-	wasImp := Column(make([]float64, num))
-	colChanged := false
-
-	intFunc := interpolate
-	if logX {
-		intFunc = interpolateLog
-	}
-
-	oldNum := len(*val)
-	oldInd := 0
-	for i, lab := range newLabel {
-		for oldInd < oldNum && lab-eps > oldLabel[oldInd] {
-			// The old label was for some reason not in the list of expected labels, so keep skipping until it works.
-			oldInd++
-		}
-		if oldInd >= oldNum || lab+eps < oldLabel[oldInd] {
-			// The current label is missing, so impute it with linear interpolation
-			if oldNum < 2 {
-				col[i] = (*val)[0]
-			} else if oldInd == 0 {
-				col[i] = intFunc(oldLabel[1], oldLabel[0], lab, (*val)[1], (*val)[0])
-			} else if oldInd >= oldNum {
-				col[i] = intFunc(oldLabel[oldNum-1], oldLabel[oldNum-2], lab, (*val)[oldNum-1], (*val)[oldNum-2])
-			} else {
-				col[i] = intFunc(oldLabel[oldInd], oldLabel[oldInd-1], lab, (*val)[oldInd], (*val)[oldInd-1])
-			}
-			wasImp[i] = 1.0
-			colChanged = true
-		} else {
-			col[i] = (*val)[oldInd]
-			oldInd++
-		}
-	}
-
-	if colChanged {
-		*val = col
-		return wasImp
-	} else {
-		return Column(nil)
-	}
-}
-
-func interpolate(x1, x2, x3, y1, y2 float64) float64 {
-	return y2 - (x2-x3)/(x1-x2)*(y1-y2)
-}
-
-func interpolateLog(x1, x2, x3, y1, y2 float64) float64 {
-	x1 = math.Log10(x1)
-	x2 = math.Log10(x2)
-	x3 = math.Log10(x3)
-	return y2 - (x2-x3)/(x1-x2)*(y1-y2)
 }
 
 func (tab Table) MarshalJSON() ([]byte, error) {
@@ -176,24 +118,4 @@ func (ts TableSet) String() string {
 		return fmt.Sprintf("TableSet{%d x ?}", ts.ColCount)
 	}
 	return fmt.Sprintf("TableSet{%dx%d}", ts.ColCount, len(ts.Tables[0][0]))
-}
-
-func (col Column) Maximum() float64 {
-	max := math.Inf(-1)
-	for _, val := range col {
-		if val > max {
-			max = val
-		}
-	}
-	return max
-}
-
-func (col Column) Minimum() float64 {
-	min := math.Inf(1)
-	for _, val := range col {
-		if val < min {
-			min = val
-		}
-	}
-	return min
 }
