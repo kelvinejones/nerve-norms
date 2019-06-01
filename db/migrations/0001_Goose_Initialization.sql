953d080f3f6d815d3dfc9d7682122de740aa3d5e
James Bell <james@bellstone.ca>
2019-06-01 09:11:59 -0600

    Remove unused database files

--- db/migrations/0001_Goose_Initialization.sql
+++
@@ -1,9 +0,0 @@
--- This serves the purpose of testing the connection and goose setup on a fresh database.  Does nothing
-
--- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
-;
-
--- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
-;
