ac466f61a694617af48a6c3fa9d8425a9b5f99e7
James Bell <james@bellstone.ca>
2019-05-31 21:17:47 -0600

    Simplify TE

--- lib/mef/te.go
+++ lib/mef/te.go
@@ -7,18 +7,12 @@ import (
 type TENorm map[string]GenericNorm
 
 func (mef *Mef) teNorm() TENorm {
-	names := []string{"h40", "h20", "d40", "d20"}
 	norm := TENorm(map[string]GenericNorm{})
-
-	for _, name := range names {
-		norm[name] = NewGenericNorm(mem.TEDelay(name), teTableForSection(name), mef)
+	for _, name := range []string{"h40", "h20", "d40", "d20"} {
+		teTableFunc := func(mData *mem.Mem) *mem.LabelledTable {
+			return (*mData.Sections["TE"].(*mem.ThresholdElectrotonus))[name]
+		}
+		norm[name] = NewGenericNorm(mem.TEDelay(name), teTableFunc, mef)
 	}
-
 	return norm
-}
-
-func teTableForSection(sec string) LabelledTableFromMem {
-	return func(mData *mem.Mem) *mem.LabelledTable {
-		return (*mData.Sections["TE"].(*mem.ThresholdElectrotonus))[sec]
-	}
 }
