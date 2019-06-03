3815e711c05a5933692743a190460fbf7f679753
James Bell <james@bellstone.ca>
2019-06-03 11:13:49 -0600

    Create a new CompositeFilter

--- lib/mef/filter.go
+++ lib/mef/filter.go
@@ -5,10 +5,30 @@ import "gogs.bellstone.ca/james/jitter/lib/mem"
 // filter returns true/false based on whether the provided Mem matches a filter
 type filter interface {
 	Apply(mem.Mem) bool
+	Combine(filter) filter
+}
+
+type CompositeFilter struct {
+	filters []filter
+}
+
+func (cf CompositeFilter) Combine(f filter) filter {
+	cf.filters = append(cf.filters, f)
+	return &cf
+}
+
+func (cf CompositeFilter) Apply(m mem.Mem) bool {
+	for _, filt := range cf.filters {
+		if !filt.Apply(m) {
+			return false
+		}
+	}
+	return true
 }
 
 // SexFilter is a type that filters sex. It uses 'mem.UnknownSex' for the unfiltered setting.
 type SexFilter struct {
+	CompositeFilter
 	mem.Sex
 }
 
@@ -18,6 +38,7 @@ func (filt SexFilter) Apply(m mem.Mem) bool {
 
 // AgeFilter is a type that filters by age. It doesn't care if oldAge<youngAge, and it considers '0' to mean a value is unset.
 type AgeFilter struct {
+	CompositeFilter
 	youngAge int
 	oldAge   int
 }
