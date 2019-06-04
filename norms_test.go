152dac998c2f2023962f42a7b84299b62edca26a
James Bell <james@bellstone.ca>
2019-06-04 15:08:02 -0600

    Ensure norms are being served

---
+++ norms_test.go
@@ -0,0 +1,26 @@
+package jitter
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNormsHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	http.HandlerFunc(NormHandler).ServeHTTP(rr, req)
+
+	status := rr.Code
+	assert.Equal(t, http.StatusOK, status)
+
+	// Just make sure something somewhat long is being printed. Great test, isn't it?
+	assert.True(t, len(rr.Body.String()) > 100)
+}
