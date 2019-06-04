6a81440745d4967630866c27e3dde78a977c34e3
James Bell <james@bellstone.ca>
2019-06-04 13:43:12 -0600

    Implement handler for outlier scores

---
+++ outliers.go
@@ -0,0 +1,66 @@
+package jitter
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"gogs.bellstone.ca/james/jitter/lib/data"
+	"gogs.bellstone.ca/james/jitter/lib/mef"
+)
+
+func OutlierScoreHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		setError(w, err.Error())
+		return
+	}
+	name := r.FormValue("name")
+	if name == "" {
+		setError(w, "Could not make outlier scores for empty participant")
+		return
+	}
+
+	var mefData mef.Mef
+	err = json.Unmarshal([]byte(data.Participants), &mefData)
+	if err != nil {
+		setError(w, err.Error())
+		return
+	}
+
+	mm := mefData.MemWithKey(name)
+	if mm == nil {
+		setError(w, "Could not find participant '"+name+"'")
+		return
+	}
+
+	sex, err := parseSex(r.FormValue("sex"))
+	if err != nil {
+		setError(w, err.Error())
+		return
+	}
+	minAge, err := strconv.Atoi(r.FormValue("minAge"))
+	if err != nil {
+		setError(w, "Could not parse minAge due to error: "+err.Error())
+		return
+	}
+	maxAge, err := strconv.Atoi(r.FormValue("maxAge"))
+	if err != nil {
+		setError(w, "Could not parse minAge due to error: "+err.Error())
+		return
+	}
+
+	mefData.Filter(mef.NewFilter().BySex(sex).ByAge(minAge, maxAge).ByCountry(r.FormValue("country")).BySpecies(r.FormValue("species")).ByNerve(r.FormValue("nerve")))
+	norm := mefData.Norm()
+	os := norm.OutlierScores(mm)
+
+	jsOSArray, err := json.Marshal(&os)
+	if err != nil {
+		setError(w, "Could not create outlier score JSON due to error: "+err.Error())
+		return
+	}
+	log.Println("Served outlier scores for " + name)
+	fmt.Fprintln(w, jsOSArray)
+}
