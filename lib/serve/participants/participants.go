78bc8443d959758178c1e91da4a1e11e0a7a8422
James Bell <james@bellstone.ca>
2019-06-04 12:22:28 -0600

    Add better participant server error handling

--- lib/serve/participants/participants.go
+++ lib/serve/participants/participants.go
@@ -3,6 +3,7 @@ package participants
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -10,14 +11,19 @@ import (
 func ParticipantHandler(w http.ResponseWriter, r *http.Request) {
 	file, err := os.Open("res/templates/data/participants.json")
 	if err != nil {
-		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		fmt.Fprintln(w, err)
 	}
 	defer file.Close()
 
 	bytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		fmt.Fprintln(w, err)
 	}
 
+	log.Println("Served participants")
 	fmt.Fprintln(w, bytes)
 }
