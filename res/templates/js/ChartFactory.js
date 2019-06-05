e8e11d944f700f53a799bfcc431095a25f45af35
James Bell <james@bellstone.ca>
2019-06-05 15:10:58 -0600

    Prevent modal on mobile

--- res/templates/js/ChartFactory.js
+++ res/templates/js/ChartFactory.js
@@ -51,7 +51,34 @@ class ChartFactory {
 		ExVars.updateValues(this.partDropDown.data)
 	}
 
+	/**
+	 * Detect the current active responsive breakpoint in Bootstrap
+	 * @returns {string}
+	 * @author farside {@link https://stackoverflow.com/users/4354249/farside}
+	 */
+	static getResponsiveBreakpoint() {
+		var envs = { xs: "d-none", sm: "d-sm-none", md: "d-md-none", lg: "d-lg-none", xl: "d-xl-none" };
+		var env = "";
+
+		var $el = $("<div>");
+		$el.appendTo($("body"));
+
+		for (var i = Object.keys(envs).length - 1; i >= 0; i--) {
+			env = Object.keys(envs)[i];
+			$el.addClass(envs[env]);
+			if ($el.is(":hidden")) {
+				break; // env detected
+			}
+		}
+		$el.remove();
+		return env;
+	};
+
 	drawModal(typeStr) {
+		if (ChartFactory.getResponsiveBreakpoint() == "xs") {
+			// Screen is too small to display this properly
+			return
+		}
 		const chart = this.build(typeStr)
 		chart.setDelayTime(Chart.fastDelay).setTransitionTime(Chart.fastTransition)
 
