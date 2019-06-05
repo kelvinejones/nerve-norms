00f77414bbfe74ed90ae9a6ec5fae0026983fcd7
James Bell <james@bellstone.ca>
2019-06-05 14:01:00 -0600

    Add missing breaks in switches

--- res/templates/js/Filter.js
+++ res/templates/js/Filter.js
@@ -6,10 +6,13 @@ class Filter {
 			switch (entry[0]) {
 				case "sex-options":
 					filter.sex = entry[1]
+					break
 				case "country-options":
 					filter.country = entry[1]
+					break
 				case "age-options":
 					Filter.setAgeOptions(filter, entry[1])
+					break
 			}
 		}
 
@@ -21,25 +24,32 @@ class Filter {
 	}
 
 	static setAgeOptions(filter, opts) {
+		console.log(opts)
 		switch (opts) {
 			case "any":
 				filter.minAge = 0
 				filter.maxAge = 200
+				break
 			case "-30":
 				filter.minAge = 0
 				filter.maxAge = 30
+				break
 			case "31-40":
 				filter.minAge = 31
 				filter.maxAge = 40
+				break
 			case "41-50":
 				filter.minAge = 41
 				filter.maxAge = 50
+				break
 			case "51-60":
 				filter.minAge = 51
 				filter.maxAge = 60
+				break
 			case "61-":
 				filter.minAge = 61
 				filter.maxAge = 200
+				break
 		}
 	}
 }
