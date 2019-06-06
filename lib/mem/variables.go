5dc094819d1c52fd88c2d6b9b5317d76be7b5444
James Bell <james@bellstone.ca>
2019-06-05 20:13:05 -0600

    Fix indices used for excitability variables

--- lib/mem/variables.go
+++ lib/mem/variables.go
@@ -20,7 +20,7 @@ type ExcitabilityVariables struct {
 	lt LabTab // Must call LoadFromMem for this to be set up
 }
 
-var expectedIndices = []int{1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38}
+var expectedIndices = []int{1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 1001, 1002, 1003}
 var ExVarIndices = Column{}
 
 func init() {
