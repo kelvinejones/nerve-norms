ab732803564b9118d73697b8872ce260acb7fd4a
James Bell <james@bellstone.ca>
2019-06-07 17:18:44 -0600

    Write code to automate update of JSON

--- cmd/combineData/combineData.go
+++ cmd/combineData/combineData.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"gogs.bellstone.ca/james/jitter/lib/mef"
 )
@@ -12,9 +13,11 @@ import (
 var caPath = flag.String("caPath", "/Users/james/Documents/Education/UofA/MSc/Research/normative-data/human/CA/FESmedianAPB.MEF", "path to the CA MEF")
 var jpPath = flag.String("jpPath", "/Users/james/Documents/Education/UofA/MSc/Research/normative-data/human/JP/JP.MEF", "path to the JP MEF")
 var poPath = flag.String("poPath", "/Users/james/Documents/Education/UofA/MSc/Research/normative-data/human/PO/Portugal.MEF", "path to the PO MEF")
-var legPath = flag.String("legPath", "/Users/james/Documents/Education/UofA/MSc/Research/normative-data/human/CA/FEScommonperonealTA.MEF", "path to the leg MEF")
+var legPath = flag.String("legPath", "", "path to the leg MEF") // /Users/james/Documents/Education/UofA/MSc/Research/normative-data/human/CA/FEScommonperonealTA.MEF
 var ratPath = flag.String("ratPath", "/Users/james/Documents/Education/UofA/MSc/Research/normative-data/rat/all.MEF", "path to the rat MEF")
 var output = flag.String("output", "json/all.json", "path to save the JSON; otherwise, output to stdout")
+var jsFile = flag.String("jsFile", "", "path to save the participants file")   // res/templates/data/participants.json
+var goFile = flag.String("goFile", "", "path to save a go file with the JSON") // lib/data/data.go
 
 func main() {
 	flag.Parse()
@@ -62,6 +65,29 @@ func main() {
 		err = ioutil.WriteFile(*output, jsArray, 0644)
 		if err != nil {
 			panic("Could not save JSON due to error: " + err.Error())
+		}
+	}
+
+	if *jsFile == "" && *goFile == "" {
+		return
+	}
+
+	jsUnescape := strings.Replace(string(jsArray), "\\\\", "\\", -1)
+
+	if *jsFile != "" {
+		err = ioutil.WriteFile(*jsFile, []byte("const participants = "+jsUnescape+"\n"), 0644)
+		if err != nil {
+			panic("Could not save jsFile due to error: " + err.Error())
+		}
+	}
+
+	if *goFile != "" {
+		goString := "package data\n\n" +
+			"var Participants = `" + jsUnescape + "`" +
+			"\nvar jsonMef = []byte(`" + string(jsArray) + "`)\n"
+		err = ioutil.WriteFile(*goFile, []byte(goString), 0644)
+		if err != nil {
+			panic("Could not save goFile due to error: " + err.Error())
 		}
 	}
 }
