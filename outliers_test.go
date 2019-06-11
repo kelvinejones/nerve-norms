514ca697a955b6fe1c5f6817fcecfbb1d347f3a2
James Bell <james@stellentus.com>
2019-06-11 13:50:21 -0600

    Add test for uploaded MEM outliers

--- outliers_test.go
+++ outliers_test.go
@@ -1,8 +1,10 @@
 package jitter
 
 import (
+	"bufio"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,7 +24,7 @@ func TestOutliersHandlerNoParticipant(t *testing.T) {
 	assert.Equal(t, http.StatusInternalServerError, status)
 }
 
-func TestOutliersHandler(t *testing.T) {
+func TestOutliersHandlerWithParticipantName(t *testing.T) {
 	req, err := http.NewRequest("GET", "?name=CA-CR21S", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -34,6 +36,25 @@ func TestOutliersHandler(t *testing.T) {
 
 	status := rr.Code
 	assert.Equal(t, http.StatusOK, status)
+
+	// Just make sure something somewhat long is being printed. Great test, isn't it?
+	assert.True(t, len(rr.Body.String()) > 100)
+}
+
+func TestConvertMemHandlerWithParticipantData(t *testing.T) {
+	file, err := os.Open("res/data/FESB70821B.MEM")
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("GET", "", bufio.NewReader(file))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	http.HandlerFunc(OutlierScoreHandler).ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
 
 	// Just make sure something somewhat long is being printed. Great test, isn't it?
 	assert.True(t, len(rr.Body.String()) > 100)
