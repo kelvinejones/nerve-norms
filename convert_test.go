d1daff40ac2071772a736a0158b62ed57915c7aa
James Bell <james@stellentus.com>
2019-06-11 10:37:26 -0600

    Add API to convert MEM to JSON (with scores)

---
+++ convert_test.go
@@ -0,0 +1,44 @@
+package jitter
+
+import (
+	"bufio"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertMemHandlerNoParticipant(t *testing.T) {
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	http.HandlerFunc(ConvertMemHandler).ServeHTTP(rr, req)
+
+	status := rr.Code
+	assert.Equal(t, http.StatusInternalServerError, status)
+}
+
+func TestConvertMemHandlerWithParticipant(t *testing.T) {
+	file, err := os.Open("res/data/FESB70821B.MEM")
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("GET", "", bufio.NewReader(file))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	http.HandlerFunc(ConvertMemHandler).ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	// Just make sure something somewhat long is being printed. Great test, isn't it?
+	assert.True(t, len(rr.Body.String()) > 100)
+}
